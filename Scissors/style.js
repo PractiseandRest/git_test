7a4a09773b4cf4839b2a91b9922646703c844beb
PractiseandRest <2966880185@qq.com>
2023-03-02 23:02:09 +0800

    add file

---
+++ Scissors/style.js
@@ -0,0 +1,17 @@
+  function getComputerChoice() {
+    return 'Scissors'
+  }
+
+ function playRound(playerSelection, computerSelection) {
+    return '你赢了'
+ }
+
+ const computerSelection = getComputerChoice()
+ const playerSelection = 'Rock'
+ console.log(playRound(playerSelection, computerSelection))
+
+ function game() {
+  for(let i = 0; i < 5; i++) {
+    
+  }
+ }
